a6938a866b8ff70207d1067bdee934bc5c0d989e
Justin Williams <williams.jman@gmail.com>
2023-01-18 14:32:49 -0500

    Added the method to get all vehicles

--- src/main/java/edu/midlands/training/services/VehicleService.java
+++ src/main/java/edu/midlands/training/services/VehicleService.java
@@ -1,4 +1,8 @@
 package edu.midlands.training.services;
 
+import edu.midlands.training.entities.Vehicle;
+import java.util.List;
+
 public interface VehicleService {
+  List<Vehicle>GetVehicle();
 }
--- src/main/java/edu/midlands/training/services/VehicleServiceImpl.java
+++ src/main/java/edu/midlands/training/services/VehicleServiceImpl.java
@@ -1,4 +1,16 @@
 package edu.midlands.training.services;
 
-public class VehicleServiceImpl {
+import edu.midlands.training.entities.Vehicle;
+import edu.midlands.training.repositories.VehicleRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class VehicleServiceImpl implements VehicleService{
+
+  @Autowired
+  private VehicleRepository vehicleRepository;
+  @Override
+  public List<Vehicle> GetVehicle() {
+    return vehicleRepository.findAll();
+  }
 }
