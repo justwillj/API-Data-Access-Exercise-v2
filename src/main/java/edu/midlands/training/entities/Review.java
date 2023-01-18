463280aa9a793cc2b391a5944142b438a0e8e290
satya mukkavilli <satyasmukkavilli@gmail.com>
2023-01-18 12:48:07 -0500

    Added getters and setters for the Review entity

--- src/main/java/edu/midlands/training/entities/Review.java
+++ src/main/java/edu/midlands/training/entities/Review.java
@@ -1,4 +1,108 @@
 package edu.midlands.training.entities;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
 public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private String rating;
+
+    private Date date;
+
+    private String username;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn
+    private Vehicle vehicle;
+
+    public Review() {
+    }
+
+    public Review(Long id, String title, String description, String rating, Date date, String username, Vehicle vehicle) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.rating = rating;
+        this.date = date;
+        this.username = username;
+        this.vehicle = vehicle;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", rating='" + rating + '\'' +
+                ", date=" + date +
+                ", username='" + username + '\'' +
+                ", vehicle=" + vehicle +
+                '}';
+    }
 }
