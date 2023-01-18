b39146e7943b4fa90df2c0bef83a98e49258e939
Justin Williams <williams.jman@gmail.com>
2023-01-18 12:28:15 -0500

    Added the structure for the vehicle entity

--- src/main/java/edu/midlands/training/entities/Vehicle.java
+++ src/main/java/edu/midlands/training/entities/Vehicle.java
@@ -1,4 +1,24 @@
 package edu.midlands.training.entities;
 
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Vehicle {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String type;
+
+  private String make;
+
+  private String model;
+
+  private String Year;
+
 }
