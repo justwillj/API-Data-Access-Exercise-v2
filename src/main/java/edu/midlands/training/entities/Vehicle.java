67f811809f04676034bc95fb330c793bfc61acab
Justin Williams <williams.jman@gmail.com>
2023-01-18 12:33:15 -0500

    Added the relationship for the vehicle class

--- src/main/java/edu/midlands/training/entities/Vehicle.java
+++ src/main/java/edu/midlands/training/entities/Vehicle.java
@@ -1,10 +1,14 @@
 package edu.midlands.training.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.Set;
 import javax.annotation.Generated;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Vehicle {
@@ -20,5 +24,9 @@ public class Vehicle {
   private String model;
 
   private String Year;
+
+  @OneToMany(mappedBy = "vehicle", fetch = FetchType.EAGER)
+  @JsonIgnore
+  private Set<Review> reviews;
 
 }
