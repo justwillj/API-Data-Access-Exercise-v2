edb92c2150bac0ede30848815ff419542c8e08da
Justin Williams <williams.jman@gmail.com>
2023-01-18 12:38:40 -0500

    Created the getters and setters for the vehicle class

--- src/main/java/edu/midlands/training/entities/Vehicle.java
+++ src/main/java/edu/midlands/training/entities/Vehicle.java
@@ -23,10 +23,73 @@ public class Vehicle {
 
   private String model;
 
-  private String Year;
+  private Integer year;
 
   @OneToMany(mappedBy = "vehicle", fetch = FetchType.EAGER)
   @JsonIgnore
   private Set<Review> reviews;
 
+
+  public Vehicle() {
+  }
+
+  public Vehicle(Long id, String type, String make, String model, Integer year) {
+    this.id = id;
+    this.type = type;
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public String getMake() {
+    return make;
+  }
+
+  public void setMake(String make) {
+    this.make = make;
+  }
+
+  public String getModel() {
+    return model;
+  }
+
+  public void setModel(String model) {
+    this.model = model;
+  }
+
+  public Integer getYear() {
+    return year;
+  }
+
+  public void setYear(Integer year) {
+    this.year = year;
+  }
+
+  @Override
+  public String toString() {
+    return "Vehicle{" +
+        "id=" + id +
+        ", type='" + type + '\'' +
+        ", make='" + make + '\'' +
+        ", model='" + model + '\'' +
+        ", year=" + year +
+        ", reviews=" + reviews +
+        '}';
+  }
 }
