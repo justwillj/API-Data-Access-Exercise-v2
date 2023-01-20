70f9c0f3ff44818be2e702d38366f808f6e36520
Andrew Grant <amg012904@gmail.com>
2023-01-20 12:57:52 -0500

    added validation handler for exceptions

---
+++ src/main/java/edu/midlands/training/exceptions/Validation.java
@@ -0,0 +1,25 @@
+package edu.midlands.training.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Validation {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldType = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldType, errorMessage);
+        });
+        return errors;
+    }
+}
